0faad835a6396a0ef68b192e18d2f568f6b3f13c
Nimisha Mohapatra <51197219+Nimisha-97@users.noreply.github.com>
2019-07-23 17:10:41 +0530

    Update server.py

--- server.py
+++ server.py
@@ -11,4 +11,4 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=os.environ.get('PORT', 3000), debug=True)
+    app.run(host='0.0.0.0', port=os.environ.get('PORT', 5000), debug=True)
